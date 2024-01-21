252884240848c87dc742790107240385f98faf2d
David Kaufmann <david@kaufmann.dev>
2024-01-21 20:28:09 +0100

    add password verification

--- unlock.js
+++ unlock.js
@@ -1,4 +1,4 @@
-$('.lockedElement').each(function() {
+$('.locked').each(function() {
     let w = $(this).outerWidth();
     let h = $(this).outerHeight();
     let title = $(this).data('title');
@@ -9,6 +9,28 @@ $('.lockedElement').each(function() {
     $(this).find('.unlock').click(function() {
         $(this).parent().parent().after(cache);
         $(this).parent().parent().remove();
+    });
+    $(this).replaceWith($(this).find('.unlockWrapper'));
+    
+});
+
+$('.passwordSecured').each(function() {
+    let w = $(this).outerWidth();
+    let h = $(this).outerHeight();
+    let title = $(this).data('title');
+    let cache = $(this);
+    
+    $(this).append('<div class="unlockWrapper" style="width: ' + w + 'px; height: ' + h + 'px"><div class="child"><p>' + title + '</p><input type="password" class="passwordInput"><button class="validateButton">Validate!</button></div></div>');
+
+    $(this).find('.validateButton').click(function() {
+        if($(this).parent().find('.passwordInput').val() == '1234') {
+            console.log("Successfully validated!");
+            $(this).parent().parent().after(cache);
+            $(this).parent().parent().remove();
+        } else {
+            console.log("Incorrect password!");
+            $(this).parent().parent().effect( "shake", {times:2, distance: 6}, 300 );
+        }
     });
     $(this).replaceWith($(this).find('.unlockWrapper'));
 });
